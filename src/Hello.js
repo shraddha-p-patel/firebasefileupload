a787599098178b6708aac7a023565b83c1404018
shraddha <pshraddha.webappdev@gmail.com>
2022-09-23 18:27:47 +0530

    hiiiiii

--- src/Hello.js
+++ src/Hello.js
@@ -3,9 +3,9 @@ import React from "react";
 const Hello = () => {
   return (
     <div>
-      <h1>hello</h1>
+      {/* <h1>hello</h1>
       <h2> Hii</h2>
-      {/* <h3>How r u?</h3> */}
+      <h3>How r u?</h3> */}
     </div>
   );
 };
